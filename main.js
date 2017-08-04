eae3c97dabdd8993bc72250d95c6ba2b86fdb057
Tomasz Drwięga <tomusdrw@gmail.com>
2017-08-04 16:04:16 +0200

    Generowanie w pętli. [Loop]

--- main.js
+++ main.js
@@ -1,37 +1,58 @@
-var activity = {
-  id: 7,
-  alt: 'Swimmer',
-  name: 'Swimming',
-  timeSpent: 60
-}
-
-var $activity = document.createElement('div')
-$activity.className = 'activity'
-
-var $img = document.createElement('img')
-$img.classList.add('activity__img')
-$img.width = 250
-$img.height = 250
-// Zastępujemy zahardcodowane wartości referencjami
-$img.alt = activity.alt
-$img.src = `https://xpla.org/ext/lorempixel/250/250/sports/${activity.id}/`
-
-var $title = document.createElement('h3')
-$title.classList.add('activity__name')
-$title.textContent = activity.name
-
-var $time = document.createElement('p')
-$time.classList.add('activity__description')
-$time.innerHTML = `Time spent: <strong>${activity.timeSpent} min</strong>`
-
-var $button = document.createElement('button')
-$button.classList.add('activity__button--paused')
-$button.innerHTML = '&#9654; Start'
-
-$activity.appendChild($img)
-$activity.appendChild($title)
-$activity.appendChild($time)
-$activity.appendChild($button)
+// 20/ Dane wrzucamy do tablicy
+var activities = [
+  {
+    id: 3,
+    alt: 'Bicycle',
+    name: 'Cycling',
+    timeSpent: 120
+  },
+  {
+    id: 7,
+    alt: 'Swimmer',
+    name: 'Swimming',
+    timeSpent: 60
+  },
+  {
+    id: 9,
+    alt: 'Runners',
+    name: 'Running',
+    timeSpent: 30
+  }
+]
 
 var $activities = document.querySelector('.activities')
-$activities.appendChild($activity)
+
+// 2/ i wyświetlamy aktywności dodająć je w pętli...
+for (var k in activities) {
+  var activity = activities[k]
+
+  var $activity = document.createElement('div')
+  $activity.className = 'activity'
+
+  var $img = document.createElement('img')
+  $img.classList.add('activity__img')
+  $img.width = 250
+  $img.height = 250
+  $img.alt = activity.alt
+  $img.src = `https://xpla.org/ext/lorempixel/250/250/sports/${activity.id}/`
+
+  var $title = document.createElement('h3')
+  $title.classList.add('activity__name')
+  $title.textContent = activity.name
+
+  var $time = document.createElement('p')
+  $time.classList.add('activity__description')
+  $time.innerHTML = `Time spent: <strong>${activity.timeSpent} min</strong>`
+
+  var $button = document.createElement('button')
+  $button.classList.add('activity__button--paused')
+  $button.innerHTML = '&#9654; Start'
+
+  $activity.appendChild($img)
+  $activity.appendChild($title)
+  $activity.appendChild($time)
+  $activity.appendChild($button)
+
+  // ...w każdej iteracji dodając do DOM
+  $activities.appendChild($activity)
+}
