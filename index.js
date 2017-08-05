a174ce7908e5edeba92d1ef7e82187b83912864b
Tomasz Drwięga <tomusdrw@gmail.com>
2017-08-05 08:50:56 +0200

    Routing

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 //#!/usr/bin/env node
 
-// Za pomocą `require` możemy zaimportować wbudowane moduły i biblioteki
 const http = require('http')
-// Node v8.x ma pełne wsparcie dla ES6/ES2015 więc możemy korzystać np. z `const`
 const port = process.env.PORT || 3000
 
-// 3/ Tworzymy serwer, który zawsze odpowiada `Hello World`...
 const server = http.createServer((req, res) => {
-  res.end('Hello World!')
+  // Za pomocą destructuring assignment wyciągamy URL
+  const { url } = req
+
+  // Logujemy na konsolę
+  console.log(url)
+
+  // 11/ Tworzymy prosty "router"
+  if (url === '/' || url === '/index.html') {
+    // 4/ Serwujemy plik HTML dla / i /index.html
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end('<h1>Hello World!</h1>')
+  } else {
+    // 2/ Zwracamy 404 w każdym innym przypadku
+    res.writeHead(404)
+    res.end('404: Not Found')
+  }
 })
 
-// 3/ ...i rozpoczynamy nasłuchiwanie na określonym porcie.
 server.listen(port, () => {
   console.log(`Listening on :${port}`)
 })
