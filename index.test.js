473805b848b3f46d54ed0bd453e0fc9bb42b0336
Tomasz Drwięga <tomusdrw@gmail.com>
2017-08-05 23:11:29 +0200

    Wprowadzamy express [express]

---
+++ index.test.js
@@ -0,0 +1,35 @@
+/* eslint-env mocha */
+
+const request = require('supertest')
+const expect = require('chai').expect
+
+const app = require('./app')
+
+describe('Server', () => {
+  it('should respond to GET /version', () => {
+    return request(app)
+      .get('/version')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        expect(response.body.version).to.equal('1.0.0')
+      })
+  })
+
+  it('should serve HTML at /', () => {
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+  })
+
+  it('should serve list of activities at /api/activities', () => {
+    return request(app)
+      .get('/api/activities')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        expect(response.body.length).to.equal(3)
+      })
+  })
+})
