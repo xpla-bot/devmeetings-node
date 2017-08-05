71753890f812ed16f5aecb1323ef6bc8200ddf3f
Tomasz Drwięga <tomusdrw@gmail.com>
2017-08-05 10:01:56 +0200

    Czytanie pliku [fs]

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 //#!/usr/bin/env node
 
 const http = require('http')
+// Moduł `fs` pozwoli nam czytać pliki
+const fs = require('fs')
+
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  // Za pomocą destructuring assignment wyciągamy URL
   const { url } = req
 
-  // Logujemy na konsolę
   console.log(url)
 
-  // 11/ Tworzymy prosty "router"
   if (url === '/' || url === '/index.html') {
-    // 4/ Serwujemy plik HTML dla / i /index.html
     res.writeHead(200, {
       'Content-Type': 'text/html'
     })
-    res.end('<h1>Hello World!</h1>')
+    // Wczytujemy plik *synchronicznie*
+    res.end(fs.readFileSync('./static/index.html', 'utf8'))
   } else {
-    // 2/ Zwracamy 404 w każdym innym przypadku
     res.writeHead(404)
     res.end('404: Not Found')
   }
